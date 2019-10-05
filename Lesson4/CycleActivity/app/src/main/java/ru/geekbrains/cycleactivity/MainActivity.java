13039cc7c591b3cb6ed7f8fbc9ee68a704389897
Natan <n-k86@mail.ru>
2019-10-05 15:27:55 +0300

    Hw 4

--- Lesson4/CycleActivity/app/src/main/java/ru/geekbrains/cycleactivity/MainActivity.java
+++ Lesson4/CycleActivity/app/src/main/java/ru/geekbrains/cycleactivity/MainActivity.java
@@ -1,13 +1,14 @@
 package ru.geekbrains.cycleactivity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnButtonClickListener {
 
     private String TAG = "[CycleActivity]";
     private TextView firstRunTextView;
@@ -75,6 +76,15 @@ public class MainActivity extends AppCompatActivity {
         makeMessage("onDestroy()");
     }
 
+    public void onClickOpenFragment(View v) {
+        //Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+        Fragment fragment = MainFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment)
+                .show(fragment)
+                .commit();
+    }
+
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonExit:
@@ -88,5 +98,15 @@ public class MainActivity extends AppCompatActivity {
     private void makeMessage(String message){
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         Log.i(TAG, message);
+    }
+
+    @Override
+    public void onFragmentButtonClick() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .remove(fragment)
+                    .commit();
+        }
     }
 }
--- Lesson4/Service/app/src/main/java/ru/geekbrains/service/MainActivity.java
+++ Lesson4/Service/app/src/main/java/ru/geekbrains/service/MainActivity.java
@@ -1,6 +1,9 @@
 package ru.geekbrains.service;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,16 +11,41 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean serviceBound = false;
+    private MainService mainService;
+    private int notifyId = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button buttonStartService = (Button) findViewById(R.id.buttonStartService);
+
+        connectService();
+
+        Button buttonStartService = findViewById(R.id.buttonStartService);
         buttonStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(new Intent(MainActivity.this, MainService.class));
+                if (serviceBound) {
+                    mainService.showNotification("Message " + notifyId, R.drawable.ic_announcement_black_24dp, notifyId++);
+                }
             }
         });
+    }
+
+    private void connectService() {
+        Intent intent = new Intent(MainActivity.this, MainService.class);
+        ServiceConnection mainServiceConnection = new ServiceConnection() {
+            public void onServiceConnected(ComponentName cName, IBinder service) {
+                MainService.ServiceBinder binder = (MainService.ServiceBinder) service;
+                mainService = binder.getService();
+                serviceBound = true;
+            }
+
+            public void onServiceDisconnected(ComponentName cName) {
+                serviceBound = false;
+            }
+        };
+        bindService(intent, mainServiceConnection, BIND_AUTO_CREATE);
     }
 }
